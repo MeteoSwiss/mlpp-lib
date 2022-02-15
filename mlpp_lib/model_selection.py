77580dd58ec4e77e19fa835b419b5c66fe652244
frazane <zanetta.francesco@gmail.com>
2022-02-15 12:04:12 +0100

    add cross-validation split

--- mlpp_lib/model_selection.py
+++ mlpp_lib/model_selection.py
@@ -1,5 +1,12 @@
+from multiprocessing.sharedctypes import Value
 import random
-from typing import Optional
+from typing import Optional, Any
+import time
+import itertools
+
+import numpy as np
+import pandas as pd
+from scipy.stats import wasserstein_distance
 
 
 def split_list(
@@ -52,3 +59,119 @@ def split_list_stratify(
         data_split.append([item for sublist in split_label_data for item in sublist[i]])
 
     return data_split
+
+
+def split_reftimes_cv(
+    reftimes: np.ndarray[Any, np.datetime64],
+    gap: int = 5,
+    interval: int = 360,
+    p: list = [0.6, 0.2, 0.2],
+    p_tol: float = 0.05,
+    uni_tol: float = 0.01,
+):
+    """Find all possible splits for a given time array and split configuration"""
+
+    if sum(p) != 1:
+        raise ValueError("Proportions do not sum up to 1!")
+
+    opt_p = np.array(p[:2]) / (1 - p[2])
+    opt_reftimes, test_reftimes = np.array_split(
+        reftimes, [int(len(reftimes) * (1 - p[2]))]
+    )
+
+    gap = np.array(gap, dtype="timedelta64[D]")
+    interval = np.array(interval, dtype="timedelta64[D]")
+    n_intervals = int((opt_reftimes[-1] - opt_reftimes[0]) / interval)
+
+    splits = []
+    nudge = [-1, 0, 1] if int(n_intervals * opt_p[1]) > 1 else [0, 1]
+    for i in nudge:
+        which = np.array(
+            list(
+                itertools.combinations(
+                    range(n_intervals), int(n_intervals * opt_p[1] + i)
+                )
+            ),
+            dtype=int,
+        )
+        splits_ = np.zeros((len(which), n_intervals), dtype=int)
+        splits_[np.arange(len(which))[None].T, which] = 1
+        splits.append(splits_)
+
+    splits = np.vstack(splits)
+
+    stop_time = time.time() + 20
+    good_splits = []
+    for choice_array in splits:
+        split = pd.Series(-1, index=opt_reftimes, dtype=int)
+        t = opt_reftimes[0]
+        for idx, choice in enumerate(choice_array):
+            next_choice = choice_array[min(idx + 1, len(choice_array) - 1)]
+            start = t
+            end = t + interval
+            split.loc[start:end] = choice
+            if next_choice != choice:
+                gap_start = end
+                gap_end = end + gap
+                split.loc[gap_start:gap_end] = 3
+            t += interval + gap
+
+        # assign last chunk
+        split.loc[opt_reftimes[-1] - gap : opt_reftimes[-1]] = 3
+        if check_split(split, opt_p, p_tol, uni_tol):
+            good_splits.append(split.values)
+        elif len(good_splits) == 30 or time.time() > stop_time:
+            break
+        else:
+            pass
+
+    if len(good_splits) == 0:
+        raise RuntimeError("No valid splits were found. Change your parameters!")
+
+    for i in range(len(good_splits)):
+        good_splits[i] = np.hstack((good_splits[i], [2] * len(test_reftimes)))
+
+    return good_splits
+
+
+def check_split(split, p, p_tol, uni_tol):
+    """Check that desired criteria are met for a given set"""
+
+    proportion = _check_proportions(split, p, p_tol)
+    uniformity = _check_uniformity(split, p, uni_tol)
+
+    return proportion and uniformity
+
+
+def _check_proportions(split, p, p_tol):
+    """Check that proportions are respected between sets"""
+
+    train_times = split[split == 0]
+    train_proportion = len(train_times) / len(split)
+    if np.isclose(train_proportion, p[0], atol=p_tol):
+        return True
+    else:
+        return False
+
+
+def _check_uniformity(split, p, uni_tol):
+    """Check that reftimes are uniformly distributed in the smaller set"""
+
+    smaller_set_ = np.argmin(p)
+
+    smaller_set = split[split == smaller_set_]
+    months = smaller_set.index.month
+    month_counts = smaller_set.groupby(months).count()
+    dist = month_counts / smaller_set.count()
+
+    uniform_dist = np.ones(12) / 12
+
+    try:
+        wsd = wasserstein_distance(dist, uniform_dist)
+    except ValueError:
+        return False
+
+    if wsd < uni_tol:
+        return True
+    else:
+        return False
