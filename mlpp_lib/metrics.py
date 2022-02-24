c54e8b4b6e85c6dc3c43bb37bc3abc5dbcac7e29
Francesco Zanetta <62377868+frazane@users.noreply.github.com>
2022-02-24 19:43:32 +0100

    refactor crps and update docstrings

--- mlpp_lib/metrics.py
+++ mlpp_lib/metrics.py
@@ -5,42 +5,56 @@ import tensorflow as tf
 import tensorflow_probability as tfp
 
 
-def crps_nrg_ensemble(
+def crps_energy_ensemble(
     fct_ensemble: Union[tf.Tensor, np.ndarray], obs: Union[tf.Tensor, np.ndarray]
 ) -> tf.Tensor:
     """
     Energy form of the Continuous Ranked Probability Score from Gneiting and Raftery (2007),
-    where the expectations are calculated from an ensemble.
+    where the expectations terms are approximated from an ensemble.
 
     .. math::
         CRPS(F, y) = E_F|X - y| - 1/2 * E_F|X - X'|
-
+    
+    Parameters
+    ----------
+    fct_ensemble: array-like
+        Ensemble forecasts, with ensemble members along the first dimension.
+    obs: array-like
+        Observations.
+    
     """
 
     # first term
-    diff = tf.abs(fct_ensemble - obs[None, :])
-    diff = tf.reduce_mean(diff, axis=0)
+    E_1 = tf.abs(fct_ensemble - obs[None, :])
+    E_1 = tf.reduce_mean(E_1, axis=0)
 
     # second term
-    forecasts_diff = tf.abs(fct_ensemble[None, :] - fct_ensemble[:, None])
-    forecasts_diff = 0.5 * tf.reduce_mean(forecasts_diff, axis=0)
+    E_2 = tf.abs(fct_ensemble[None, :] - fct_ensemble[:, None])
+    E_2 = tf.reduce_mean(E_2, axis=0)
 
-    return diff - forecasts_diff
+    return E_1 - E_2 / 2
 
 
-def crps_nrg(
+def crps_energy(
     fct_dist: tfp.distributions.Distribution, obs: Union[tf.Tensor, np.ndarray]
 ) -> tf.Tensor:
     """
     Energy form of the Continuous Ranked Probability Score from Gneiting and Raftery (2007),
-    where the expectations are calculated from the distribution itself.
+    where the expectation terms are approximated from the distribution using monte-carlo methods. 
 
     .. math::
         CRPS(F, x) = E_F|X - x| - 1/2 * E_F|X - X'|
-
+        
+    Parameters
+    ----------
+    fct_dist: tensorflow-probability Distribution
+        The predicted distribution.
+    obs: array-like
+        Observations.
+        
     """
 
-    n_samples = 10000
+    n_samples = 1000
 
     obs = tf.debugging.check_numerics(obs, "Target values")
 
