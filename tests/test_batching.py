d056aa16562f3e4b2f462f846606a2b31166a4ed
frazane <zanetta.francesco@gmail.com>
2022-03-11 14:59:41 +0100

    update tests

--- tests/test_batching.py
+++ tests/test_batching.py
@@ -6,21 +6,17 @@ import pytest
 from mlpp_lib.batching import get_tensor_dataset
 
 
-@pytest.mark.parametrize("event_dims,", ([], ["t"]))
+@pytest.mark.parametrize("event_dims,", [[], ["t"]])
 def test_get_tensor_dataset(features_dataset, targets_dataset, event_dims):
 
-    batch_size = 32
-    batched_dataset = get_tensor_dataset(
-        features_dataset, targets_dataset, event_dims, batch_size
-    )
+    tensor_dataset = get_tensor_dataset(features_dataset, targets_dataset, event_dims)
 
     event_dims_size = [features_dataset.dims.mapping[dim] for dim in event_dims]
     expected_batch_x_shape = (None, *event_dims_size, len(features_dataset.data_vars))
     expected_batch_y_shape = (None, *event_dims_size, len(targets_dataset.data_vars))
 
-    element_spec = batched_dataset.element_spec
-    batch_x_shape = tuple(element_spec[0].shape)
-    batch_y_shape = tuple(element_spec[1].shape)
+    x_shape = tensor_dataset[0].shape
+    y_shape = tensor_dataset[1].shape
 
-    assert batch_x_shape == expected_batch_x_shape
-    assert batch_y_shape == expected_batch_y_shape
+    assert x_shape[1:] == expected_batch_x_shape[1:]
+    assert y_shape[1:] == expected_batch_y_shape[1:]
