b270f25af9a736fcb57d3d4315732e0ea8eac386
ned <daniele.nerini@meteoswiss.ch>
2022-03-24 12:28:00 +0100

    Add IndependentGamma layer and make MultivariateNormalTriL serializible

--- mlpp_lib/probabilistic_layers.py
+++ mlpp_lib/probabilistic_layers.py
@@ -1,15 +1,169 @@
 """In this module, any custom built keras layers are included."""
 import numpy as np
 import tensorflow as tf
-import tensorflow_probability as tfp
+import tensorflow_probability.python.layers as tfpl
 from tensorflow_probability.python import bijectors as tfb
 from tensorflow_probability.python import distributions as tfd
+from tensorflow_probability.python.layers.distribution_layer import (
+    _event_size,
+    _get_convert_to_tensor_fn,
+    _serialize,
+    dist_util,
+    independent_lib,
+)
 
-# ready to use
+
+# these work out of the box
 from tensorflow_probability.python.layers import (
     IndependentNormal,
     IndependentLogistic,
     IndependentBernoulli,
     IndependentPoisson,
-    MultivariateNormalTriL,
 )
+
+
+@tf.keras.utils.register_keras_serializable()
+class MultivariateNormalTriL(tfpl.MultivariateNormalTriL):
+    def __init__(
+        self,
+        event_size,
+        convert_to_tensor_fn=tfd.Distribution.sample,
+        validate_args=False,
+        **kwargs
+    ):
+        convert_to_tensor_fn = _get_convert_to_tensor_fn(convert_to_tensor_fn)
+
+        # If there is a 'make_distribution_fn' keyword argument (e.g., because we
+        # are being called from a `from_config` method), remove it.  We pass the
+        # distribution function to `DistributionLambda.__init__` below as the first
+        # positional argument.
+        kwargs.pop("make_distribution_fn", None)
+
+        super().__init__(event_size, convert_to_tensor_fn, validate_args, **kwargs)
+        self._event_size = event_size
+        self._convert_to_tensor_fn = convert_to_tensor_fn
+        self._validate_args = validate_args
+
+    def get_config(self):
+        """Returns the config of this layer.
+        NOTE: At the moment, this configuration can only be serialized if the
+        Layer's `convert_to_tensor_fn` is a serializable Keras object (i.e.,
+        implements `get_config`) or one of the standard values:
+        - `Distribution.sample` (or `"sample"`)
+        - `Distribution.mean` (or `"mean"`)
+        - `Distribution.mode` (or `"mode"`)
+        - `Distribution.stddev` (or `"stddev"`)
+        - `Distribution.variance` (or `"variance"`)
+        """
+        config = {
+            "event_size": self._event_size,
+            "convert_to_tensor_fn": _serialize(self._convert_to_tensor_fn),
+            "validate_args": self._validate_args,
+        }
+        base_config = super(MultivariateNormalTriL, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+
+@tf.keras.utils.register_keras_serializable()
+class IndependentGamma(tfpl.DistributionLambda):
+    """An independent gamma Keras layer."""
+
+    def __init__(
+        self,
+        event_shape=(),
+        convert_to_tensor_fn=tfd.Distribution.sample,
+        validate_args=False,
+        **kwargs
+    ):
+        """Initialize the `IndependentGamma` layer.
+        Args:
+        event_shape: integer vector `Tensor` representing the shape of single
+            draw from this distribution.
+        convert_to_tensor_fn: Python `callable` that takes a `tfd.Distribution`
+            instance and returns a `tf.Tensor`-like object.
+            Default value: `tfd.Distribution.sample`.
+        validate_args: Python `bool`, default `False`. When `True` distribution
+            parameters are checked for validity despite possibly degrading runtime
+            performance. When `False` invalid inputs may silently render incorrect
+            outputs.
+            Default value: `False`.
+        **kwargs: Additional keyword arguments passed to `tf.keras.Layer`.
+        """
+        convert_to_tensor_fn = _get_convert_to_tensor_fn(convert_to_tensor_fn)
+
+        # If there is a 'make_distribution_fn' keyword argument (e.g., because we
+        # are being called from a `from_config` method), remove it.  We pass the
+        # distribution function to `DistributionLambda.__init__` below as the first
+        # positional argument.
+        kwargs.pop("make_distribution_fn", None)
+
+        super(IndependentGamma, self).__init__(
+            lambda t: IndependentGamma.new(t, event_shape, validate_args),
+            convert_to_tensor_fn,
+            **kwargs
+        )
+
+        self._event_shape = event_shape
+        self._convert_to_tensor_fn = convert_to_tensor_fn
+        self._validate_args = validate_args
+
+    @staticmethod
+    def new(params, event_shape=(), validate_args=False, name=None):
+        """Create the distribution instance from a `params` vector."""
+        with tf.name_scope(name or "IndependentGamma"):
+            params = tf.convert_to_tensor(params, name="params")
+            event_shape = dist_util.expand_to_vector(
+                tf.convert_to_tensor(
+                    event_shape, name="event_shape", dtype_hint=tf.int32
+                ),
+                tensor_name="event_shape",
+            )
+            output_shape = tf.concat(
+                [
+                    tf.shape(params)[:-1],
+                    event_shape,
+                ],
+                axis=0,
+            )
+            concentration, rate = tf.split(params, 2, axis=-1)
+            return independent_lib.Independent(
+                tfd.Gamma(
+                    concentration=tf.math.softplus(
+                        tf.reshape(concentration, output_shape)
+                    ),
+                    rate=tf.math.softplus(tf.reshape(rate, output_shape)),
+                    validate_args=validate_args,
+                ),
+                reinterpreted_batch_ndims=tf.size(event_shape),
+                validate_args=validate_args,
+            )
+
+    @staticmethod
+    def params_size(event_shape=(), name=None):
+        """The number of `params` needed to create a single distribution."""
+        with tf.name_scope(name or "IndependentGamma_params_size"):
+            event_shape = tf.convert_to_tensor(
+                event_shape, name="event_shape", dtype_hint=tf.int32
+            )
+            return np.int32(2) * _event_size(
+                event_shape, name=name or "IndependentGamma_params_size"
+            )
+
+    def get_config(self):
+        """Returns the config of this layer.
+        NOTE: At the moment, this configuration can only be serialized if the
+        Layer's `convert_to_tensor_fn` is a serializable Keras object (i.e.,
+        implements `get_config`) or one of the standard values:
+        - `Distribution.sample` (or `"sample"`)
+        - `Distribution.mean` (or `"mean"`)
+        - `Distribution.mode` (or `"mode"`)
+        - `Distribution.stddev` (or `"stddev"`)
+        - `Distribution.variance` (or `"variance"`)
+        """
+        config = {
+            "event_shape": self._event_shape,
+            "convert_to_tensor_fn": _serialize(self._convert_to_tensor_fn),
+            "validate_args": self._validate_args,
+        }
+        base_config = super(IndependentGamma, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
--- tests/test_probabilistic_layers.py
+++ tests/test_probabilistic_layers.py
@@ -1,9 +1,12 @@
 from inspect import getmembers
 
+import pytest
 import tensorflow as tf
+from keras.engine.functional import Functional
+from tensorflow.python.framework.errors_impl import OperatorNotAllowedInGraphError
+
 from mlpp_lib import probabilistic_layers
 
-import pytest
 
 LAYERS = [
     obj[0] for obj in getmembers(probabilistic_layers) if hasattr(obj[1], "_is_layer")
@@ -29,3 +32,49 @@ def test_probabilistic_layers(layer):
     samples = output_dist.sample(n_samples)
 
     assert samples.shape == (n_samples, 10, output_size)
+
+
+@pytest.mark.parametrize("layer", LAYERS)
+def test_probabilistic_model_compile(layer):
+    """Test model compile with prob layers"""
+
+    layer_class = getattr(probabilistic_layers, layer)
+    tfkl = tf.keras.layers
+    input_shape = [28, 28, 1]
+    encoded_shape = 2
+    encoder = tf.keras.Sequential(
+        [
+            tfkl.InputLayer(input_shape=input_shape),
+            tfkl.Flatten(),
+            tfkl.Dense(10, activation="relu"),
+            tfkl.Dense(layer_class.params_size(encoded_shape)),
+            layer_class(encoded_shape),
+        ]
+    )
+    encoder.summary()
+    encoder.compile()
+    assert isinstance(encoder, Functional)
+
+
+@pytest.mark.parametrize("layer", LAYERS)
+def test_probabilistic_model_save(layer, tmp_path):
+    """Test model save/load with prob layers"""
+    layer_class = getattr(probabilistic_layers, layer)
+    tfkl = tf.keras.layers
+    input_shape = [28, 28, 1]
+    encoded_shape = 2
+    encoder = tf.keras.Sequential(
+        [
+            tfkl.InputLayer(input_shape=input_shape),
+            tfkl.Flatten(),
+            tfkl.Dense(10, activation="relu"),
+            tfkl.Dense(layer_class.params_size(encoded_shape)),
+            layer_class(encoded_shape),
+        ]
+    )
+    assert isinstance(encoder.from_config(encoder.get_config()), Functional)
+    with pytest.raises(OperatorNotAllowedInGraphError):
+        encoder.save(tmp_path)
+    encoder.save(tmp_path, save_traces=False)
+    encoder = tf.keras.models.load_model(tmp_path)
+    assert isinstance(encoder, Functional)
