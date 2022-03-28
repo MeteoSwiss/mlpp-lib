50dd351bf823b874f75a40e971724d0be094417f
ned <daniele.nerini@meteoswiss.ch>
2022-03-28 17:13:20 +0200

    Add IndependentLogNormal layer

--- mlpp_lib/probabilistic_layers.py
+++ mlpp_lib/probabilistic_layers.py
@@ -272,3 +272,106 @@ class IndependentWeibull(tfpl.DistributionLambda):
         }
         base_config = super(IndependentWeibull, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
+
+
+@tf.keras.utils.register_keras_serializable()
+class IndependentLogNormal(tfpl.DistributionLambda):
+    """An independent LogNormal Keras layer."""
+
+    def __init__(
+        self,
+        event_shape=(),
+        convert_to_tensor_fn=tfd.Distribution.sample,
+        validate_args=False,
+        **kwargs
+    ):
+        """Initialize the `IndependentLogNormal` layer.
+        Args:
+        event_shape: integer vector `Tensor` representing the shape of single
+            draw from this distribution.
+        convert_to_tensor_fn: Python `callable` that takes a `tfd.Distribution`
+            instance and returns a `tf.Tensor`-like object.
+            Default value: `tfd.Distribution.sample`.
+        validate_args: Python `bool`, default `False`. When `True` distribution
+            parameters are checked for validity despite possibly degrading runtime
+            performance. When `False` invalid inputs may silently render incorrect
+            outputs.
+            Default value: `False`.
+        **kwargs: Additional keyword arguments passed to `tf.keras.Layer`.
+        """
+        convert_to_tensor_fn = _get_convert_to_tensor_fn(convert_to_tensor_fn)
+
+        # If there is a 'make_distribution_fn' keyword argument (e.g., because we
+        # are being called from a `from_config` method), remove it.  We pass the
+        # distribution function to `DistributionLambda.__init__` below as the first
+        # positional argument.
+        kwargs.pop("make_distribution_fn", None)
+
+        super(IndependentLogNormal, self).__init__(
+            lambda t: IndependentLogNormal.new(t, event_shape, validate_args),
+            convert_to_tensor_fn,
+            **kwargs
+        )
+
+        self._event_shape = event_shape
+        self._convert_to_tensor_fn = convert_to_tensor_fn
+        self._validate_args = validate_args
+
+    @staticmethod
+    def new(params, event_shape=(), validate_args=False, name=None):
+        """Create the distribution instance from a `params` vector."""
+        with tf.name_scope(name or "IndependentLogNormal"):
+            params = tf.convert_to_tensor(params, name="params")
+            event_shape = dist_util.expand_to_vector(
+                tf.convert_to_tensor(
+                    event_shape, name="event_shape", dtype_hint=tf.int32
+                ),
+                tensor_name="event_shape",
+            )
+            output_shape = tf.concat(
+                [
+                    tf.shape(params)[:-1],
+                    event_shape,
+                ],
+                axis=0,
+            )
+            loc, scale = tf.split(params, 2, axis=-1)
+            return independent_lib.Independent(
+                tfd.LogNormal(
+                    loc=tf.reshape(loc, output_shape),
+                    scale=tf.math.softplus(tf.reshape(scale, output_shape)),
+                    validate_args=validate_args,
+                ),
+                reinterpreted_batch_ndims=tf.size(event_shape),
+                validate_args=validate_args,
+            )
+
+    @staticmethod
+    def params_size(event_shape=(), name=None):
+        """The number of `params` needed to create a single distribution."""
+        with tf.name_scope(name or "IndependentLogNormal_params_size"):
+            event_shape = tf.convert_to_tensor(
+                event_shape, name="event_shape", dtype_hint=tf.int32
+            )
+            return np.int32(2) * _event_size(
+                event_shape, name=name or "IndependentLogNormal_params_size"
+            )
+
+    def get_config(self):
+        """Returns the config of this layer.
+        NOTE: At the moment, this configuration can only be serialized if the
+        Layer's `convert_to_tensor_fn` is a serializable Keras object (i.e.,
+        implements `get_config`) or one of the standard values:
+        - `Distribution.sample` (or `"sample"`)
+        - `Distribution.mean` (or `"mean"`)
+        - `Distribution.mode` (or `"mode"`)
+        - `Distribution.stddev` (or `"stddev"`)
+        - `Distribution.variance` (or `"variance"`)
+        """
+        config = {
+            "event_shape": self._event_shape,
+            "convert_to_tensor_fn": _serialize(self._convert_to_tensor_fn),
+            "validate_args": self._validate_args,
+        }
+        base_config = super(IndependentLogNormal, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
