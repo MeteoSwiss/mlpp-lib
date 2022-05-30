2eac5700f7876e0aeca58cece6dd2656b0c2f9fd
ned <daniele.nerini@meteoswiss.ch>
2022-05-30 12:46:28 +0200

    Test model load from separate process

--- tests/test_probabilistic_layers.py
+++ tests/test_probabilistic_layers.py
@@ -1,9 +1,10 @@
+import subprocess
 from inspect import getmembers
 
+import numpy as np
 import pytest
 import tensorflow as tf
 from keras.engine.functional import Functional
-from tensorflow.python.framework.errors_impl import OperatorNotAllowedInGraphError
 
 from mlpp_lib import models
 from mlpp_lib import probabilistic_layers
@@ -75,15 +76,40 @@ def test_probabilistic_model_predict(layer, features_dataset, targets_dataset):
     assert out.shape[2] == targets.sizes["target"]
 
 
+@pytest.mark.parametrize("save_format", ["tf", "h5"])
 @pytest.mark.parametrize("layer", LAYERS)
-def test_probabilistic_model_save(layer, tmp_path):
+def test_probabilistic_save_model(save_format, layer, tmp_path):
     """Test model save/load with prob layers"""
+
+    if save_format == "h5":
+        tmp_path = f"{tmp_path}.h5"
+        save_traces = True  # default value
+    else:
+        tmp_path = f"{tmp_path}"
+        save_traces = False
+
     model = models.fully_connected_network(
         (5,), 2, hidden_layers=[3], probabilistic_layer=layer
     )
     assert isinstance(model.from_config(model.get_config()), Functional)
-    with pytest.raises(OperatorNotAllowedInGraphError):
-        model.save(tmp_path)
-    model.save(tmp_path, save_traces=False)
+    model.compile()
+    model.save(tmp_path, save_traces=save_traces)
+
+    # test trying to load the model from a new process
+    # this is a bit slow, since each process needs to reload all the dependencies ...
+    args = [
+        "python",
+        "-c",
+        f"import tensorflow as tf; from mlpp_lib.probabilistic_layers import {layer}; tf.keras.models.load_model('{tmp_path}')",
+    ]
+    completed_process = subprocess.run(args)
+    assert completed_process.returncode == 0, "failed to reload model"
+
+    # loading here is not a good test because the custom layers are still somehow in memory
+    # we'll do it anyway to test that the behavior doesn't change after loading
+    input_arr = tf.random.uniform((1, 5))
+    outputs = model(input_arr).mean()
+    del model
     model = tf.keras.models.load_model(tmp_path)
     assert isinstance(model, Functional)
+    np.testing.assert_allclose(model(input_arr).mean(), outputs)
