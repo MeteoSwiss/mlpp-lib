8e15cc7055df5becac6e00a93dfc0153d98835ee
ned <daniele.nerini@meteoswiss.ch>
2022-09-15 15:14:46 +0200

    Add MultivariateNormalDiag probabilistic layer

--- mlpp_lib/probabilistic_layers.py
+++ mlpp_lib/probabilistic_layers.py
@@ -23,48 +23,6 @@ from tensorflow_probability.python.layers import (
 
 
 @tf.keras.utils.register_keras_serializable()
-class MultivariateNormalTriL(tfpl.MultivariateNormalTriL):
-    def __init__(
-        self,
-        event_size,
-        convert_to_tensor_fn=tfd.Distribution.mean,
-        validate_args=False,
-        **kwargs
-    ):
-        convert_to_tensor_fn = _get_convert_to_tensor_fn(convert_to_tensor_fn)
-
-        # If there is a 'make_distribution_fn' keyword argument (e.g., because we
-        # are being called from a `from_config` method), remove it.  We pass the
-        # distribution function to `DistributionLambda.__init__` below as the first
-        # positional argument.
-        kwargs.pop("make_distribution_fn", None)
-
-        super().__init__(event_size, convert_to_tensor_fn, validate_args, **kwargs)
-        self._event_size = event_size
-        self._convert_to_tensor_fn = convert_to_tensor_fn
-        self._validate_args = validate_args
-
-    def get_config(self):
-        """Returns the config of this layer.
-        NOTE: At the moment, this configuration can only be serialized if the
-        Layer's `convert_to_tensor_fn` is a serializable Keras object (i.e.,
-        implements `get_config`) or one of the standard values:
-        - `Distribution.sample` (or `"sample"`)
-        - `Distribution.mean` (or `"mean"`)
-        - `Distribution.mode` (or `"mode"`)
-        - `Distribution.stddev` (or `"stddev"`)
-        - `Distribution.variance` (or `"variance"`)
-        """
-        config = {
-            "event_size": self._event_size,
-            "convert_to_tensor_fn": _serialize(self._convert_to_tensor_fn),
-            "validate_args": self._validate_args,
-        }
-        base_config = super(MultivariateNormalTriL, self).get_config()
-        return dict(list(base_config.items()) + list(config.items()))
-
-
-@tf.keras.utils.register_keras_serializable()
 class IndependentGamma(tfpl.DistributionLambda):
     """An independent gamma Keras layer."""
 
@@ -480,4 +438,137 @@ class IndependentWeibull(tfpl.DistributionLambda):
             "validate_args": self._validate_args,
         }
         base_config = super(IndependentWeibull, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+
+@tf.keras.utils.register_keras_serializable()
+class MultivariateNormalDiag(tfpl.DistributionLambda):
+    """A `d`-variate normal Keras layer from `2* d` params,
+    with a diagonal scale matrix.
+    """
+
+    def __init__(
+        self,
+        event_size,
+        convert_to_tensor_fn=tfd.Distribution.mean,
+        validate_args=False,
+        **kwargs
+    ):
+        """Initialize the layer.
+        Args:
+            event_size: Scalar `int` representing the size of single draw from this
+              distribution.
+            convert_to_tensor_fn: Python `callable` that takes a `tfd.Distribution`
+              instance and returns a `tf.Tensor`-like object. For examples, see
+              `class` docstring.
+              Default value: `tfd.Distribution.sample`.
+            validate_args: Python `bool`, default `False`. When `True` distribution
+              parameters are checked for validity despite possibly degrading runtime
+              performance. When `False` invalid inputs may silently render incorrect
+              outputs.
+              Default value: `False`.
+            **kwargs: Additional keyword arguments passed to `tf.keras.Layer`.
+        """
+        convert_to_tensor_fn = _get_convert_to_tensor_fn(convert_to_tensor_fn)
+
+        # If there is a 'make_distribution_fn' keyword argument (e.g., because we
+        # are being called from a `from_config` method), remove it.  We pass the
+        # distribution function to `DistributionLambda.__init__` below as the first
+        # positional argument.
+        kwargs.pop("make_distribution_fn", None)
+
+        super(MultivariateNormalDiag, self).__init__(
+            lambda t: MultivariateNormalDiag.new(t, event_size, validate_args),
+            convert_to_tensor_fn,
+            **kwargs
+        )
+
+        self._event_size = event_size
+        self._convert_to_tensor_fn = convert_to_tensor_fn
+        self._validate_args = validate_args
+
+    @staticmethod
+    def new(params, event_size, validate_args=False, name=None):
+        """Create the distribution instance from a 'params' vector."""
+        with tf.name_scope(name or "MultivariateNormalDiag"):
+            params = tf.convert_to_tensor(params, name="params")
+            if event_size > 1:
+                dist = tfd.MultivariateNormalDiag(
+                    loc=params[..., :event_size],
+                    scale_diag=1e-5 + tf.math.softplus(params[..., event_size:]),
+                    validate_args=validate_args,
+                )
+            else:
+                dist = tfd.Normal(
+                    loc=params[..., :event_size],
+                    scale=1e-5 + tf.math.softplus(params[..., event_size:]),
+                    validate_args=validate_args,
+                )
+            return dist
+
+    @staticmethod
+    def params_size(event_size, name=None):
+        """The number of 'params' needed to create a single distribution."""
+        with tf.name_scope(name or "MultivariateNormalDiag_params_size"):
+            return 2 * event_size
+
+    def get_config(self):
+        """Returns the config of this layer.
+        NOTE: At the moment, this configuration can only be serialized if the
+        Layer's `convert_to_tensor_fn` is a serializable Keras object (i.e.,
+        implements `get_config`) or one of the standard values:
+        - `Distribution.sample` (or `"sample"`)
+        - `Distribution.mean` (or `"mean"`)
+        - `Distribution.mode` (or `"mode"`)
+        - `Distribution.stddev` (or `"stddev"`)
+        - `Distribution.variance` (or `"variance"`)
+        """
+        config = {
+            "event_size": self._event_size,
+            "convert_to_tensor_fn": _serialize(self._convert_to_tensor_fn),
+            "validate_args": self._validate_args,
+        }
+        base_config = super(MultivariateNormalDiag, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+
+@tf.keras.utils.register_keras_serializable()
+class MultivariateNormalTriL(tfpl.MultivariateNormalTriL):
+    def __init__(
+        self,
+        event_size,
+        convert_to_tensor_fn=tfd.Distribution.mean,
+        validate_args=False,
+        **kwargs
+    ):
+        convert_to_tensor_fn = _get_convert_to_tensor_fn(convert_to_tensor_fn)
+
+        # If there is a 'make_distribution_fn' keyword argument (e.g., because we
+        # are being called from a `from_config` method), remove it.  We pass the
+        # distribution function to `DistributionLambda.__init__` below as the first
+        # positional argument.
+        kwargs.pop("make_distribution_fn", None)
+
+        super().__init__(event_size, convert_to_tensor_fn, validate_args, **kwargs)
+        self._event_size = event_size
+        self._convert_to_tensor_fn = convert_to_tensor_fn
+        self._validate_args = validate_args
+
+    def get_config(self):
+        """Returns the config of this layer.
+        NOTE: At the moment, this configuration can only be serialized if the
+        Layer's `convert_to_tensor_fn` is a serializable Keras object (i.e.,
+        implements `get_config`) or one of the standard values:
+        - `Distribution.sample` (or `"sample"`)
+        - `Distribution.mean` (or `"mean"`)
+        - `Distribution.mode` (or `"mode"`)
+        - `Distribution.stddev` (or `"stddev"`)
+        - `Distribution.variance` (or `"variance"`)
+        """
+        config = {
+            "event_size": self._event_size,
+            "convert_to_tensor_fn": _serialize(self._convert_to_tensor_fn),
+            "validate_args": self._validate_args,
+        }
+        base_config = super(MultivariateNormalTriL, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
