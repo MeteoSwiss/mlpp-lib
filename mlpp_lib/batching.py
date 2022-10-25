e56303e17a1bdb49bcbf0ae3642cf2d636d674ef
ned <daniele.nerini@meteoswiss.ch>
2022-10-25 17:45:41 +0200

    Persist None in the output list

--- mlpp_lib/batching.py
+++ mlpp_lib/batching.py
@@ -13,17 +13,25 @@ def dataset_to_tensor(dataset: xr.Dataset, event_dims: list = []) -> xr.DataArra
     return tensor
 
 
-def get_tensor_dataset(*datasets: xr.Dataset, event_dims: list) -> list[xr.DataArray]:
-    """Convert xarray datasets into tensors and filter out samples with missing values"""
+def get_tensor_dataset(
+    *datasets: xr.Dataset or None, event_dims: list
+) -> list[xr.DataArray or None]:
+    """Convert xarray datasets into tensors and filter out samples with missing values.
+    If provided as inputs, None as persisted in the output list."""
 
     tensors = []
     mask = True
     for i, dataset in enumerate(datasets):
+        if dataset is None:
+            tensors.append(None)
+            continue
         tensors.append(dataset_to_tensor(dataset, event_dims))
         mask_dataset_dims = [dim for dim in tensors[i].dims if dim != "sample"]
         mask *= np.isfinite(tensors[i]).all(dim=mask_dataset_dims)
 
     for j, tensor in enumerate(tensors):
+        if tensor is None:
+            continue
         tensors[j] = tensor[mask]
 
     return tensors
--- tests/test_batching.py
+++ tests/test_batching.py
@@ -1,4 +1,5 @@
 import pytest
+import xarray as xr
 
 from mlpp_lib.batching import get_tensor_dataset
 
@@ -6,16 +7,26 @@ from mlpp_lib.batching import get_tensor_dataset
 @pytest.mark.parametrize("event_dims,", [[], ["t"]])
 def test_get_tensor_dataset(features_dataset, targets_dataset, event_dims):
 
-    tensor_dataset = get_tensor_dataset(
+    tensors = get_tensor_dataset(
         features_dataset, targets_dataset, event_dims=event_dims
     )
+
+    assert all([isinstance(tensor, xr.DataArray) for tensor in tensors])
 
     event_dims_size = [features_dataset.dims.mapping[dim] for dim in event_dims]
     expected_batch_x_shape = (None, *event_dims_size, len(features_dataset.data_vars))
     expected_batch_y_shape = (None, *event_dims_size, len(targets_dataset.data_vars))
 
-    x_shape = tensor_dataset[0].shape
-    y_shape = tensor_dataset[1].shape
+    x_shape = tensors[0].shape
+    y_shape = tensors[1].shape
 
     assert x_shape[1:] == expected_batch_x_shape[1:]
     assert y_shape[1:] == expected_batch_y_shape[1:]
+
+
+def test_get_tensor_dataset_with_None(features_dataset, targets_dataset):
+
+    tensor_dataset = get_tensor_dataset(
+        features_dataset, None, targets_dataset, event_dims=[]
+    )
+    assert tensor_dataset[1] is None
