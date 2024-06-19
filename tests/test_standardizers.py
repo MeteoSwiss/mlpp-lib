79d990bec16a57f12b8176fb822b8fe01dbbda59
Louis Poulain--Auzéau <louis.poulainauzeau@meteoswiss.ch>
2024-06-19 17:25:41 +0200

    Ensure fillvalue is -5 in this test

--- tests/test_standardizers.py
+++ tests/test_standardizers.py
@@ -4,15 +4,15 @@ from mlpp_lib.standardizers import Standardizer
 
 
 def test_fit(features_dataset):
-    standardizer = Standardizer()
+    standardizer = Standardizer(fillvalue=-5)
     standardizer.fit(features_dataset)
     assert all(var in standardizer.mean.data_vars for var in features_dataset.data_vars)
     assert all(var in standardizer.std.data_vars for var in features_dataset.data_vars)
-    assert standardizer.fillvalue == -10
+    assert standardizer.fillvalue == -5
 
 
 def test_transform(features_dataset):
-    standardizer = Standardizer()
+    standardizer = Standardizer(fillvalue=-5)
     standardizer.fit(features_dataset)
     ds = standardizer.transform(features_dataset)[0]
     assert all(var in ds.data_vars for var in features_dataset.data_vars)
@@ -21,12 +21,13 @@ def test_transform(features_dataset):
 
 
 def test_inverse_transform(features_dataset):
-    standardizer = Standardizer()
+    standardizer = Standardizer(fillvalue=-5)
     standardizer.fit(features_dataset)
     ds = standardizer.transform(features_dataset)[0]
     inv_ds = standardizer.inverse_transform(ds)[0]
+
     assert all(
-        np.allclose(inv_ds[var].values, features_dataset[var].values)
+        np.allclose(inv_ds[var].values, features_dataset[var].values, equal_nan=True, atol=1e-6)
         for var in features_dataset.data_vars
     )
     assert all(var in inv_ds.data_vars for var in features_dataset.data_vars)
@@ -34,7 +35,7 @@ def test_inverse_transform(features_dataset):
 
 def test_serialization(features_dataset, tmp_path):
     fn = f"{tmp_path}/standardizer.json"
-    standardizer = Standardizer()
+    standardizer = Standardizer(fillvalue=-5)
     standardizer.fit(features_dataset)
     standardizer.save_json(fn)
     new_standardizer = Standardizer.from_json(fn)
