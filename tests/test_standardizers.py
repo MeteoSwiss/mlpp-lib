0edf87dcf2dc84315325b959e613416153acbc87
louisPoulain <louis.poulainauzeau@meteoswiss.ch>
2024-06-25 16:45:50 +0200

    Use again as pytesting. Removed the serialization test as serialization was moved to the data_transformer class

--- tests/test_standardizers.py
+++ tests/test_standardizers.py
@@ -31,14 +31,3 @@ def test_inverse_transform(features_dataset):
         for var in features_dataset.data_vars
     )
     assert all(var in inv_ds.data_vars for var in features_dataset.data_vars)
-
-
-def test_serialization(features_dataset, tmp_path):
-    fn = f"{tmp_path}/standardizer.json"
-    standardizer = Standardizer(fillvalue=-5)
-    standardizer.fit(features_dataset)
-    standardizer.save_json(fn)
-    new_standardizer = Standardizer.from_json(fn)
-    assert standardizer.fillvalue == new_standardizer.fillvalue
-    assert standardizer.mean.identical(new_standardizer.mean)
-    assert standardizer.std.identical(new_standardizer.std)
