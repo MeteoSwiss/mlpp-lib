6da0d2d63634c734267610c23c4f53c12f6fd970
louisPoulain <louis.poulainauzeau@meteoswiss.ch>
2024-09-04 16:12:17 +0200

    Add a censored normal layer computed via softclipping

--- mlpp_lib/probabilistic_layers.py
+++ mlpp_lib/probabilistic_layers.py
@@ -831,6 +831,119 @@ class IndependentTruncatedNormal(tfpl.DistributionLambda):
     def output(self):
         """This allows the use of this layer with the shap package."""
         return super(IndependentTruncatedNormal, self).output[0]
+    
+
+class IndependentSoftClipCensored(tfpl.DistributionLambda):
+    """An independent censored normal Keras layer."""
+
+    def __init__(
+        self,
+        event_shape=(),
+        convert_to_tensor_fn=tfd.Distribution.mean,
+        validate_args=False,
+        **kwargs
+    ):
+        """Initialize the `IndependentSoftClipCensored` layer.
+        Args:
+        event_shape: integer vector `Tensor` representing the shape of single
+            draw from this distribution.
+        convert_to_tensor_fn: Python `callable` that takes a `tfd.Distribution`
+            instance and returns a `tf.Tensor`-like object.
+            Default value: `tfd.Distribution.mean`.
+        validate_args: Python `bool`, default `False`. When `True` distribution
+            parameters are checked for validity despite possibly degrading runtime
+            performance. When `False` invalid inputs may silently render incorrect
+            outputs.
+            Default value: `False`.
+        **kwargs: Additional keyword arguments passed to `tf.keras.Layer`.
+        """
+        convert_to_tensor_fn = _get_convert_to_tensor_fn(convert_to_tensor_fn)
+
+        # If there is a 'make_distribution_fn' keyword argument (e.g., because we
+        # are being called from a `from_config` method), remove it.  We pass the
+        # distribution function to `DistributionLambda.__init__` below as the first
+        # positional argument.
+        kwargs.pop("make_distribution_fn", None)
+
+        super(IndependentSoftClipCensored, self).__init__(
+            lambda t: IndependentSoftClipCensored.new(t, event_shape, validate_args),
+            convert_to_tensor_fn,
+            **kwargs
+        )
+
+        self._event_shape = event_shape
+        self._convert_to_tensor_fn = convert_to_tensor_fn
+        self._validate_args = validate_args
+
+    @staticmethod
+    def new(params, event_shape=(), validate_args=False, name=None):
+        """Create the distribution instance from a `params` vector."""
+        with tf.name_scope(name or "IndependentSoftClipCensored"):
+            params = tf.convert_to_tensor(params, name="params")
+            event_shape = dist_util.expand_to_vector(
+                tf.convert_to_tensor(
+                    event_shape, name="event_shape", dtype_hint=tf.int32
+                ),
+                tensor_name="event_shape",
+            )
+            output_shape = tf.concat(
+                [
+                    tf.shape(params)[:-1],
+                    event_shape,
+                ],
+                axis=0,
+            )
+            loc, scale = tf.split(params, 2, axis=-1)
+            loc = tf.reshape(loc, output_shape)
+            scale = tf.math.softplus(tf.reshape(scale, output_shape)) + 1e-3
+            normal_dist = tfd.Normal(
+                loc=loc, scale=scale, validate_args=validate_args
+            )
+
+            clipped_dist = tfd.TransformedDistribution(
+                distribution=normal_dist, bijector=tfb.SoftClip(low=0.0, high=1.0, hinge_softness=0.5)
+            )
+
+            return independent_lib.Independent(
+                clipped_dist,
+                reinterpreted_batch_ndims=tf.size(event_shape),
+                validate_args=validate_args,
+            )
+
+    @staticmethod
+    def params_size(event_shape=(), name=None):
+        """The number of `params` needed to create a single distribution."""
+        with tf.name_scope(name or "IndependentSoftClipCensored_params_size"):
+            event_shape = tf.convert_to_tensor(
+                event_shape, name="event_shape", dtype_hint=tf.int32
+            )
+            return np.int32(2) * _event_size(
+                event_shape, name=name or "IndependentSoftClipCensored_params_size"
+            )
+
+    def get_config(self):
+        """Returns the config of this layer.
+        NOTE: At the moment, this configuration can only be serialized if the
+        Layer's `convert_to_tensor_fn` is a serializable Keras object (i.e.,
+        implements `get_config`) or one of the standard values:
+        - `Distribution.sample` (or `"sample"`)
+        - `Distribution.mean` (or `"mean"`)
+        - `Distribution.mode` (or `"mode"`)
+        - `Distribution.stddev` (or `"stddev"`)
+        - `Distribution.variance` (or `"variance"`)
+        """
+        config = {
+            "event_shape": self._event_shape,
+            "convert_to_tensor_fn": _serialize(self._convert_to_tensor_fn),
+            "validate_args": self._validate_args,
+        }
+        base_config = super(IndependentSoftClipCensored, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+    @property
+    def output(self):
+        """This allows the use of this layer with the shap package."""
+        return super(IndependentSoftClipCensored, self).output[0]
 
 
 @tf.keras.saving.register_keras_serializable()
