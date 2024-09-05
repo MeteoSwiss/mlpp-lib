98dc6d15a78e8f581f83258dadbb87fb63aee701
ned <daniele.nerini@meteoswiss.ch>
2024-09-05 14:43:16 +0200

    Add LogitNormal layer

--- mlpp_lib/probabilistic_layers.py
+++ mlpp_lib/probabilistic_layers.py
@@ -21,6 +21,7 @@ from tensorflow_probability.python.layers import (
     IndependentPoisson,
 )
 
+
 @tf.keras.saving.register_keras_serializable()
 class IndependentNormal(IndependentNormal):
     @property
@@ -269,7 +270,7 @@ class Independent4ParamsBeta(tfpl.DistributionLambda):
     def output(self):
         """This allows the use of this layer with the shap package."""
         return super(Independent4ParamsBeta, self).output[0]
-    
+
 
 @tf.keras.saving.register_keras_serializable()
 class IndependentCensoredNormal(tfpl.DistributionLambda):
@@ -335,9 +336,7 @@ class IndependentCensoredNormal(tfpl.DistributionLambda):
             loc, scale = tf.split(params, 2, axis=-1)
             loc = tf.reshape(loc, output_shape)
             scale = tf.math.softplus(tf.reshape(scale, output_shape)) + 1e-6
-            normal_dist = tfd.Normal(
-                loc=loc, scale=scale, validate_args=validate_args
-            )
+            normal_dist = tfd.Normal(loc=loc, scale=scale, validate_args=validate_args)
 
             class CustomCensored(tfd.Distribution):
                 def __init__(self, normal):
@@ -350,10 +349,10 @@ class IndependentCensoredNormal(tfpl.DistributionLambda):
                     )
 
                 def _sample_n(self, n, seed=None):
-                    
+
                     # Sample from normal distribution
                     samples = self.normal.sample(sample_shape=(n,), seed=seed)
-                    
+
                     # Clip values between 0 and 1
                     chosen_samples = tf.clip_by_value(samples, 0, 1)
 
@@ -370,12 +369,16 @@ class IndependentCensoredNormal(tfpl.DistributionLambda):
                     self.low_bound_pdf = self.normal.prob(low_bound_standard)
                     self.high_bound_pdf = self.normal.prob(high_bound_standard)
 
-                    return original_mean + self.normal.stddev() * (self.low_bound_pdf - self.high_bound_pdf) / (self.high_bound_cdf - self.low_bound_cdf + 1e-3)
+                    return original_mean + self.normal.stddev() * (
+                        self.low_bound_pdf - self.high_bound_pdf
+                    ) / (self.high_bound_cdf - self.low_bound_cdf + 1e-3)
 
                 def _log_prob(self, value):
                     original_log_prob = self.normal.log_prob(value)
-                    
-                    return original_log_prob - tf.math.log(self.high_bound_cdf - self.low_bound_cdf + 1e-3)
+
+                    return original_log_prob - tf.math.log(
+                        self.high_bound_cdf - self.low_bound_cdf + 1e-3
+                    )
 
             return independent_lib.Independent(
                 CustomCensored(normal_dist),
@@ -417,11 +420,12 @@ class IndependentCensoredNormal(tfpl.DistributionLambda):
     def output(self):
         """This allows the use of this layer with the shap package."""
         return super(IndependentCensoredNormal, self).output[0]
-    
+
 
 @tf.keras.saving.register_keras_serializable()
 class IndependentConcaveBeta(tfpl.DistributionLambda):
     """An independent 4-parameter Beta Keras layer with enforced concavity"""
+
     # INdependent
     def __init__(
         self,
@@ -530,7 +534,7 @@ class IndependentConcaveBeta(tfpl.DistributionLambda):
     def output(self):
         """This allows the use of this layer with the shap package."""
         return super(IndependentConcaveBeta, self).output[0]
-    
+
 
 @tf.keras.saving.register_keras_serializable()
 class IndependentGamma(tfpl.DistributionLambda):
@@ -751,6 +755,114 @@ class IndependentLogNormal(tfpl.DistributionLambda):
 
 
 @tf.keras.saving.register_keras_serializable()
+class IndependentLogitNormal(tfpl.DistributionLambda):
+    """An independent Logit-Normal Keras layer."""
+
+    def __init__(
+        self,
+        event_shape=(),
+        convert_to_tensor_fn=tfd.Distribution.sample,
+        validate_args=False,
+        **kwargs
+    ):
+        """Initialize the `IndependentLogitNormal` layer.
+        Args:
+        event_shape: integer vector `Tensor` representing the shape of single
+            draw from this distribution.
+        convert_to_tensor_fn: Python `callable` that takes a `tfd.Distribution`
+            instance and returns a `tf.Tensor`-like object.
+            Default value: `tfd.Distribution.mean`.
+        validate_args: Python `bool`, default `False`. When `True` distribution
+            parameters are checked for validity despite possibly degrading runtime
+            performance. When `False` invalid inputs may silently render incorrect
+            outputs.
+            Default value: `False`.
+        **kwargs: Additional keyword arguments passed to `tf.keras.Layer`.
+        """
+        convert_to_tensor_fn = _get_convert_to_tensor_fn(convert_to_tensor_fn)
+
+        # If there is a 'make_distribution_fn' keyword argument (e.g., because we
+        # are being called from a `from_config` method), remove it.  We pass the
+        # distribution function to `DistributionLambda.__init__` below as the first
+        # positional argument.
+        kwargs.pop("make_distribution_fn", None)
+
+        super(IndependentLogitNormal, self).__init__(
+            lambda t: IndependentLogitNormal.new(t, event_shape, validate_args),
+            convert_to_tensor_fn,
+            **kwargs
+        )
+
+        self._event_shape = event_shape
+        self._convert_to_tensor_fn = convert_to_tensor_fn
+        self._validate_args = validate_args
+
+    @staticmethod
+    def new(params, event_shape=(), validate_args=False, name=None):
+        """Create the distribution instance from a `params` vector."""
+        with tf.name_scope(name or "IndependentLogitNormal"):
+            params = tf.convert_to_tensor(params, name="params")
+            event_shape = dist_util.expand_to_vector(
+                tf.convert_to_tensor(
+                    event_shape, name="event_shape", dtype_hint=tf.int32
+                ),
+                tensor_name="event_shape",
+            )
+            output_shape = tf.concat(
+                [
+                    tf.shape(params)[:-1],
+                    event_shape,
+                ],
+                axis=0,
+            )
+            loc, scale = tf.split(params, 2, axis=-1)
+            return independent_lib.Independent(
+                tfd.LogitNormal(
+                    loc=tf.reshape(loc, output_shape),
+                    scale=tf.math.softplus(tf.reshape(scale, output_shape)) + 1e-3,
+                    validate_args=validate_args,
+                ),
+                reinterpreted_batch_ndims=tf.size(event_shape),
+                validate_args=validate_args,
+            )
+
+    @staticmethod
+    def params_size(event_shape=(), name=None):
+        """The number of `params` needed to create a single distribution."""
+        with tf.name_scope(name or "IndependentLogitNormal_params_size"):
+            event_shape = tf.convert_to_tensor(
+                event_shape, name="event_shape", dtype_hint=tf.int32
+            )
+            return np.int32(2) * _event_size(
+                event_shape, name=name or "IndependentLogitNormal_params_size"
+            )
+
+    def get_config(self):
+        """Returns the config of this layer.
+        NOTE: At the moment, this configuration can only be serialized if the
+        Layer's `convert_to_tensor_fn` is a serializable Keras object (i.e.,
+        implements `get_config`) or one of the standard values:
+        - `Distribution.sample` (or `"sample"`)
+        - `Distribution.mean` (or `"mean"`)
+        - `Distribution.mode` (or `"mode"`)
+        - `Distribution.stddev` (or `"stddev"`)
+        - `Distribution.variance` (or `"variance"`)
+        """
+        config = {
+            "event_shape": self._event_shape,
+            "convert_to_tensor_fn": _serialize(self._convert_to_tensor_fn),
+            "validate_args": self._validate_args,
+        }
+        base_config = super(IndependentLogitNormal, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+    @property
+    def output(self):
+        """This allows the use of this layer with the shap package."""
+        return super(IndependentLogitNormal, self).output[0]
+
+
+@tf.keras.saving.register_keras_serializable()
 class IndependentTruncatedNormal(tfpl.DistributionLambda):
     """An independent TruncatedNormal Keras layer."""
 
@@ -858,9 +970,8 @@ class IndependentTruncatedNormal(tfpl.DistributionLambda):
     def output(self):
         """This allows the use of this layer with the shap package."""
         return super(IndependentTruncatedNormal, self).output[0]
-    
-    
-    
+
+
 @tf.keras.saving.register_keras_serializable()
 class IndependentSoftClipCensored(tfpl.DistributionLambda):
     """An independent censored normal Keras layer."""
@@ -925,12 +1036,11 @@ class IndependentSoftClipCensored(tfpl.DistributionLambda):
             loc, scale = tf.split(params, 2, axis=-1)
             loc = tf.reshape(loc, output_shape)
             scale = tf.math.softplus(tf.reshape(scale, output_shape)) + 1e-3
-            normal_dist = tfd.Normal(
-                loc=loc, scale=scale, validate_args=validate_args
-            )
+            normal_dist = tfd.Normal(loc=loc, scale=scale, validate_args=validate_args)
 
             clipped_dist = tfd.TransformedDistribution(
-                distribution=normal_dist, bijector=tfb.SoftClip(low=0.0, high=1.0, hinge_softness=0.5)
+                distribution=normal_dist,
+                bijector=tfb.SoftClip(low=0.0, high=1.0, hinge_softness=0.5),
             )
 
             return independent_lib.Independent(
@@ -1084,12 +1194,12 @@ class IndependentWeibull(tfpl.DistributionLambda):
     def output(self):
         """This allows the use of this layer with the shap package."""
         return super(IndependentWeibull, self).output[0]
-    
+
 
 @tf.keras.saving.register_keras_serializable()
 class MixtureTruncatedNormal(tfpl.DistributionLambda):
-    """ A mixture of two truncated normal distributions Keras layer.
-        5-parameters distribution: loc1, scale1, loc2, scale2, weight
+    """A mixture of two truncated normal distributions Keras layer.
+    5-parameters distribution: loc1, scale1, loc2, scale2, weight
     """
 
     def __init__(
@@ -1137,14 +1247,14 @@ class MixtureTruncatedNormal(tfpl.DistributionLambda):
         """Create the distribution instance from a `params` vector."""
         with tf.name_scope(name or "MixtureTruncatedNormal"):
             params = tf.convert_to_tensor(params, name="params")
-            
+
             event_shape = dist_util.expand_to_vector(
                 tf.convert_to_tensor(
                     event_shape, name="event_shape", dtype_hint=tf.int32
                 ),
                 tensor_name="event_shape",
             )
-            
+
             # Ensure the event shape is correctly handled
             output_shape = tf.concat(
                 [
@@ -1153,21 +1263,30 @@ class MixtureTruncatedNormal(tfpl.DistributionLambda):
                 ],
                 axis=0,
             )
-            
+
             loc1, scale1, loc2, scale2, weight = tf.split(params, 5, axis=-1)
             loc1 = tf.reshape(loc1, output_shape)
             scale1 = tf.math.softplus(tf.reshape(scale1, output_shape)) + 1e-3
             loc2 = tf.reshape(loc2, output_shape)
             scale2 = tf.math.softplus(tf.reshape(scale2, output_shape)) + 1e-3
             weight = tf.math.sigmoid(tf.reshape(weight, output_shape))
-            
+
             # Create the component distributions
-            trunc_normal1 = tfd.TruncatedNormal(loc=loc1, scale=scale1, low=0.0, high=1.0)
-            trunc_normal2 = tfd.TruncatedNormal(loc=loc2, scale=scale2, low=0.0, high=1.0)
-            
+            trunc_normal1 = tfd.TruncatedNormal(
+                loc=loc1, scale=scale1, low=0.0, high=1.0
+            )
+            trunc_normal2 = tfd.TruncatedNormal(
+                loc=loc2, scale=scale2, low=0.0, high=1.0
+            )
+
             # Create a categorical distribution for the weights
-            cat = tfd.Categorical(probs=tf.concat([tf.expand_dims(weight, -1), tf.expand_dims(1-weight, -1)], axis=-1))
-            
+            cat = tfd.Categorical(
+                probs=tf.concat(
+                    [tf.expand_dims(weight, -1), tf.expand_dims(1 - weight, -1)],
+                    axis=-1,
+                )
+            )
+
             class CustomMixture(tfd.Distribution):
                 def __init__(self, cat, trunc_normal1, trunc_normal2):
                     self.cat = cat
@@ -1182,16 +1301,20 @@ class MixtureTruncatedNormal(tfpl.DistributionLambda):
 
                 def _sample_n(self, n, seed=None):
                     indices = self.cat.sample(sample_shape=(n,), seed=seed)
-                    
+
                     # Sample from both truncated normal distributions
                     samples1 = self.trunc_normal1.sample(sample_shape=(n,), seed=seed)
                     samples2 = self.trunc_normal2.sample(sample_shape=(n,), seed=seed)
-                    
+
                     # Stack the samples along a new axis
                     samples = tf.stack([samples1, samples2], axis=-1)
-                    
+
                     # Gather samples according to indices from the categorical distribution
-                    chosen_samples = tf.gather(samples, indices, batch_dims=tf.get_static_value(tf.rank(indices)))
+                    chosen_samples = tf.gather(
+                        samples,
+                        indices,
+                        batch_dims=tf.get_static_value(tf.rank(indices)),
+                    )
 
                     return chosen_samples
 
@@ -1199,20 +1322,25 @@ class MixtureTruncatedNormal(tfpl.DistributionLambda):
                     log_prob1 = self.trunc_normal1.log_prob(value)
                     log_prob2 = self.trunc_normal2.log_prob(value)
                     log_probs = tf.stack([log_prob1, log_prob2], axis=-1)
-                    weighted_log_probs = log_probs + tf.math.log(tf.concat([weight, 1 - weight], axis=-1))
+                    weighted_log_probs = log_probs + tf.math.log(
+                        tf.concat([weight, 1 - weight], axis=-1)
+                    )
                     return tf.reduce_logsumexp(weighted_log_probs, axis=-1)
 
                 def _mean(self):
-                    return (weight * self.trunc_normal1.mean() + (1 - weight) * self.trunc_normal2.mean())
+                    return (
+                        weight * self.trunc_normal1.mean()
+                        + (1 - weight) * self.trunc_normal2.mean()
+                    )
 
             mixture_dist = CustomMixture(cat, trunc_normal1, trunc_normal2)
-            
+
             return independent_lib.Independent(
                 mixture_dist,
                 reinterpreted_batch_ndims=tf.size(event_shape),
                 validate_args=validate_args,
             )
-        
+
     @staticmethod
     def params_size(event_shape=(), name=None):
         """The number of `params` needed to create a single distribution."""
@@ -1223,7 +1351,7 @@ class MixtureTruncatedNormal(tfpl.DistributionLambda):
             return np.int32(5) * _event_size(
                 event_shape, name=name or "MixtureTruncatedNormal_params_size"
             )
-        
+
     def get_config(self):
         """Returns the config of this layer.
         NOTE: At the moment, this configuration can only be serialized if the
@@ -1242,7 +1370,7 @@ class MixtureTruncatedNormal(tfpl.DistributionLambda):
         }
         base_config = super(MixtureTruncatedNormal, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
-    
+
     @property
     def output(self):
         """This allows the use of this layer with the shap package."""
