Fix logprob definition

mlpp_lib/probabilistic_layers.py
@@ -386,11 +386,16 @@ class IndependentDoublyCensoredNormal(tfpl.DistributionLambda):
                             pdf(low_bound_standard) - pdf(high_bound_standard))
 
                 def _log_prob(self, value):
-                    original_log_prob = self.normal.log_prob(value)
+                    
+                    mu, sigma = self.normal.mean(), self.normal.stddev()
+                    cdf = lambda x: tfd.Normal(0, 1).cdf(x)
+                    pdf = lambda x: tfd.Normal(0, 1).prob(x)
+                    
+                    logprob_left = lambda x: tf.log(cdf(-mu / sigma) + 1e-3)
+                    logprob_middle = lambda x: self.normal.log_prob(x)
+                    logprob_right = lambda x: tf.log(1 - cdf((1 - mu) / sigma) + 1e-3)
 
-                    return original_log_prob - tf.math.log(
-                        self.high_bound_cdf - self.low_bound_cdf + 1e-3
-                    )
+                    return logprob_left(value) + logprob_middle(value) + logprob_right(value)
 
             return independent_lib.Independent(
                 CustomCensored(normal_dist),