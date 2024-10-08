08b5a4db0f86f2403b7ad6a2589c3f941a243939
Louis Poulain--Auzéau <47986600+louisPoulain@users.noreply.github.com>
2024-10-08 11:33:57 +0200

    Fix NaN bug in validation (#59)
    * Change from checking for NaNs to checking non finite values (Inf, -Inf, NaN)
    
    * Fix - Check for non finite
    
    * Remove debugging log
    
    * Remove comment for another PR
    
    ---------
    
    Co-authored-by: Louis Poulain--Auzéau <louis.poulainauzeau@meteoswiss.ch>

--- mlpp_lib/datasets.py
+++ mlpp_lib/datasets.py
@@ -449,9 +449,9 @@ class Dataset:
         x, y, w = self._get_copies()
 
         event_axes = [self.dims.index(dim) for dim in self.dims if dim != "s"]
-        mask = da.any(da.isnan(da.from_array(x, name="x")), axis=event_axes)
+        mask = da.any(~da.isfinite(da.from_array(x, name="x")), axis=event_axes)
         if y is not None:
-            mask = mask | da.any(da.isnan(da.from_array(y, name="y")), axis=event_axes)
+            mask = mask | da.any(~da.isfinite(da.from_array(y, name="y")), axis=event_axes)
         mask = (~mask).compute()
 
         # with grouped samples, nans have to be removed in blocks:
