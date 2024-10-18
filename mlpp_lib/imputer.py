00c203db89d4eda40cc55cb916968980ba92117a
Louis Poulain--Auzéau <47986600+louisPoulain@users.noreply.github.com>
2024-10-18 16:40:35 +0200

    Prepare a template for imputation methods

--- mlpp_lib/imputer.py
+++ mlpp_lib/imputer.py
@@ -6,7 +6,18 @@ import numpy as np
 import xarray as xr
 from typing_extensions import Self
 
+_LOGGER = logging.getLogger(__name__)
 
+
+def create_transformation_from_str(class_name: str, inputs: Optional[dict] = None):
+    """"""
+    cls = globals().get(class_name)
+    if not cls or not issubclass(cls, DataImputation):
+        raise ValueError(f"{class_name} is not a valid subclass of DataImputation")
+    return cls(**(inputs or {}))
+
+
+@dataclass
 class Imputer:
     """
     Abstract class for filling missing values in a dataset.
@@ -25,14 +36,43 @@ class Imputer:
         """
         pass
 
+class DataImputation:
+    pass
+
 
 @dataclass
-class ConstantFiller(ValueFiller):
+class ConstantImputation(DataImputation):
     """
     Fill missing values with a constant value.
     """
 
     fillvalue: dict[str, float] = field(init=True, default=-5)
+    name = "ConstantImputation"
 
-    def fill(self, data: xr.Dataset) -> xr.Dataset:
-        return data.fillna(self.fillvalue)
+    def fill(self, *datasets: xr.Dataset) -> tuple[xr.Dataset, ...]:
+        
+        def f(ds: xr.Dataset):
+            ds = ds.copy()
+            ds = ds.fillna(self.fillvalue)
+            return ds.astype("float32")
+
+        return tuple(f(ds) for ds in datasets)
+
+    def inverse_fill(self, *datasets: xr.Dataset) -> tuple[xr.Dataset, ...]:
+        def f(ds: xr.Dataset) -> xr.Dataset:
+            ds = ds.copy()
+            ds = ds.where(ds > self.fillvalue)
+            return ds.astype("float32")
+
+        return tuple(f(ds) for ds in datasets)
+
+    @classmethod
+    def from_dict(cls, in_dict: dict) -> Self:
+        fillvalue = in_dict["fillvalue"]
+        return cls(fillvalue=fillvalue)
+
+    def to_dict(self):
+        out_dict = {
+            "fillvalue": self.fillvalue,
+        }
+        return out_dict
