f8826e008a63a69b6f71d665c60079dfe8cec832
Daniele Nerini <daniele.nerini@gmail.com>
2025-08-05 22:14:27 +0200

    Remove properscoring

--- mlpp_lib/callbacks.py
+++ mlpp_lib/callbacks.py
@@ -1,7 +1,7 @@
 import time
 
 import numpy as np
-import properscoring as ps
+import scoringrules as sr
 from keras import callbacks
 
 
@@ -23,17 +23,22 @@ class EnsembleMetrics(callbacks.Callback):
         assert y_val.shape[0] == y_pred.shape[0]
         assert y_pred.shape[1] == self.n_samples
 
+        def exceedances(x, thr):
+            exceeds = (x > thr).astype(float)
+            exceeds[np.where(np.isnan(x))] = np.nan
+            return exceeds
+
         logs["val_ensstd"] = np.std(y_pred, axis=1).mean().astype(float)
-        logs["val_crps"] = ps.crps_ensemble(y_val, y_pred, axis=1).mean()
+        logs["val_crps"] = sr.crps_ensemble(y_val, y_pred, m_axis=1).mean()
         for thr in self.thresholds:
             y_val_thr = np.maximum(y_val, thr)
             y_pred_thr = np.maximum(y_pred, thr)
-            logs[f"val_crps_{thr}"] = ps.crps_ensemble(
-                y_val_thr, y_pred_thr, axis=1
+            logs[f"val_crps_{thr}"] = sr.crps_ensemble(
+                y_val_thr, y_pred_thr, m_axis=1
             ).mean()
-            logs[f"val_bs_{thr}"] = ps.threshold_brier_score(
-                y_val, y_pred, threshold=thr, axis=1
-            ).mean()
+            y_val_bin = exceedances(y_val, thr)
+            y_pred_prob = exceedances(y_pred, thr).mean(axis=1)
+            logs[f"val_bs_{thr}"] = sr.brier_score(y_val_bin, y_pred_prob).mean()
 
 
 class TimeHistory(callbacks.Callback):
